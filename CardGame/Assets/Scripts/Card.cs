fb4e4ffd23c58ac95fef7bb0320e5a981fc58220
davidfrk <davida1d2@gmail.com>
2017-12-09 10:53:42 -0200

    Lógica base do jogador e cartas. Iniciando parte visual das cartas

--- CardGame/Assets/Scripts/Card.cs
+++ CardGame/Assets/Scripts/Card.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Card : MonoBehaviour {
 
-    public Stats Cost;
-    public Stats UserEffects;
+    public int Cost;
+    public StatsType CostType;
+
     public bool hasTarget = true;
-    public Stats TargetEffects;
+    public int EffectStrength = 1;
+    public int SecondaryEffectStrength = 0;
+    public TargetPlayers targetPlayers;
+    public TargetStats targetStats;
 
     public bool isPlayable(Player Owner)
     {
-        return Owner.stats.isGreater(Cost);
+        return (Owner.stats.GetStat(CostType) >= Cost);
     }
 
     public void Activate(Player Owner, Player Target)
@@ -21,11 +25,134 @@ public class Card : MonoBehaviour {
             Debug.LogError("Null Target when activating effect");
             return;
         }
-
-        Owner.ApplyStats(UserEffects);
-        if (Target != null)
+        Owner.ApplyStats(-Cost, CostType);
+        
+        switch (targetPlayers)
         {
-            Target.ApplyStats(TargetEffects);
+            case TargetPlayers.Owner:
+                {
+                    ApplyEffect(Owner, 1);
+                    break;
+                }
+            case TargetPlayers.Target:
+                {
+                    ApplyEffect(Target, 1);
+                    break;
+                }
+            case TargetPlayers.Both:
+                {
+                    ApplyEffect(Owner, 1);
+                    ApplyEffect(Target, 1);
+                    break;
+                }
+            case TargetPlayers.Steal:
+                {
+                    ApplyEffect(Owner, 1);
+                    ApplyEffect(Target, -1);
+                    break;
+                }
+            default:
+                break;
         }
     }
+
+    void ApplyEffect(Player Target, int Multiplier)
+    {
+        switch (targetStats)
+        {
+            case TargetStats.Hp:
+                {
+                    Target.stats.Hp += EffectStrength * Multiplier;
+                    break;
+                }
+            case TargetStats.Defense:
+                {
+                    Target.stats.Defense += EffectStrength * Multiplier;
+                    break;
+                }
+            case TargetStats.Workers:
+                {
+                    Target.stats.Workers += EffectStrength * Multiplier;
+                    break;
+                }
+            case TargetStats.Materials:
+                {
+                    Target.stats.Materials += EffectStrength * Multiplier;
+                    break;
+                }
+            case TargetStats.Generals:
+                {
+                    Target.stats.Generals += EffectStrength * Multiplier;
+                    break;
+                }
+            case TargetStats.Soldiers:
+                {
+                    Target.stats.Soldiers += EffectStrength * Multiplier;
+                    break;
+                }
+            case TargetStats.Mages:
+                {
+                    Target.stats.Mages += EffectStrength * Multiplier;
+                    break;
+                }
+            case TargetStats.Magic:
+                {
+                    Target.stats.Magic += EffectStrength * Multiplier;
+                    break;
+                }
+            case TargetStats.Stocks:
+                {
+                    Target.stats.Materials += EffectStrength * Multiplier;
+                    Target.stats.Soldiers += EffectStrength * Multiplier;
+                    Target.stats.Magic += EffectStrength * Multiplier;
+                    break;
+                }
+            case TargetStats.Attack:
+                {
+                    int damage = EffectStrength - Target.stats.Defense;
+                    damage = Mathf.Max(0, damage);
+
+                    Target.stats.Defense -= EffectStrength;
+                    Target.stats.Hp -= damage;
+                    break;
+                }
+            case TargetStats.All:
+                {
+                    Target.stats.Hp += EffectStrength * Multiplier;
+                    Target.stats.Defense += EffectStrength * Multiplier;
+                    Target.stats.Workers += EffectStrength * Multiplier;
+                    Target.stats.Materials += EffectStrength * Multiplier;
+                    Target.stats.Generals += EffectStrength * Multiplier;
+                    Target.stats.Soldiers += EffectStrength * Multiplier;
+                    Target.stats.Mages += EffectStrength * Multiplier;
+                    Target.stats.Magic += EffectStrength * Multiplier;
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+}
+
+public enum TargetPlayers
+{
+    Owner,
+    Target,
+    Both,
+    Steal
+}
+
+public enum TargetStats
+{
+    Hp,
+    Defense,
+    Workers,
+    Materials,
+    Generals,
+    Soldiers,
+    Mages,
+    Magic,
+    Stocks,
+    Attack,
+    All,
 }
--- CardGame/Assets/Scripts/Player.cs
+++ CardGame/Assets/Scripts/Player.cs
@@ -29,27 +29,29 @@ public class Player : MonoBehaviour {
     {
         stats.Add(Effect);
     }
+
+    public void ApplyStats(int Amount, StatsType statsType)
+    {
+        stats.ApplyStats(Amount, statsType);
+    }
 }
 
 [System.Serializable]
 public class Stats
 {
-    public int hp;
-    public int Defense;
-    public int Workers;
-    public int Materials;
-    public int Generals;
-    public int Soldiers;
-    public int Mages;
-    public int Magic;
+    private int hp;
+    private int defense;
+    private int workers;
+    private int materials;
+    private int generals;
+    private int soldiers;
+    private int mages;
+    private int magic;
 
     public int Hp
     {
-        get
-        {
-            return hp;
-        }
-        private set
+        get { return hp; }
+        set
         {
             if (value <= 0)
             {
@@ -68,10 +70,52 @@ public class Stats
         }
     }
 
+    public int Defense
+    {
+        get { return defense; }
+        set { defense = Mathf.Clamp(value, 0, 100); }
+    }
+
+    public int Workers
+    {
+        get { return workers; }
+        set { workers = Mathf.Clamp(value, 0, 100); }
+    }
+
+    public int Materials
+    {
+        get { return materials; }
+        set { materials = Mathf.Clamp(value, 0, 100); }
+    }
+
+    public int Generals
+    {
+        get { return generals; }
+        set { generals = Mathf.Clamp(value, 0, 100); }
+    }
+
+    public int Soldiers
+    {
+        get { return soldiers; }
+        set { soldiers = Mathf.Clamp(value, 0, 100); }
+    }
+
+    public int Mages
+    {
+        get { return mages; }
+        set { mages = Mathf.Clamp(value, 0, 100); }
+    }
+
+    public int Magic
+    {
+        get { return magic; }
+        set { defense = Mathf.Clamp(value, 0, 100); }
+    }
+
     public void Set(int Hp, int Defense, int Workers, int Materials, int Generals, int Soldiers, int Mages, int Magic)
     {
         this.Hp = Hp;
-        this.Defense = Mathf.Clamp(Defense, 0, 100);
+        this.Defense = Defense;
         this.Workers = Workers;
         this.Materials = Materials;
         this.Generals = Generals;
@@ -97,14 +141,6 @@ public class Stats
         Soldiers += stats.Soldiers;
         Mages += stats.Mages;
         Magic += stats.Magic;
-
-        Defense = Mathf.Clamp(Defense, 0, 100);
-        if (Workers < 0) Workers = 0;
-        if (Materials < 0) Materials = 0;
-        if (Generals < 0) Generals = 0;
-        if (Soldiers < 0) Soldiers = 0;
-        if (Mages < 0) Mages = 0;
-        if (Magic < 0) Magic = 0;
     }
 
     public bool isGreater(Stats other)
@@ -112,4 +148,107 @@ public class Stats
         return (hp >= other.hp && Defense >= other.Defense && Workers >= other.Workers && Materials >= other.Materials &&
                 Generals >= other.Generals && Soldiers >= other.Soldiers && Mages >= other.Mages && Magic >= other.Magic);
     }
+
+    public void ApplyStats(int Amount, StatsType statsType)
+    {
+        switch (statsType)
+        {
+            case StatsType.Hp:
+                {
+                    Hp += Amount;
+                    break;
+                }
+            case StatsType.Defense:
+                {
+                    Defense += Amount;
+                    break;
+                }
+            case StatsType.Workers:
+                {
+                    Workers += Amount;
+                    break;
+                }
+            case StatsType.Materials:
+                {
+                    Materials += Amount;
+                    break;
+                }
+            case StatsType.Generals:
+                {
+                    Generals += Amount;
+                    break;
+                }
+            case StatsType.Soldiers:
+                {
+                    Soldiers += Amount;
+                    break;
+                }
+            case StatsType.Mages:
+                {
+                    Mages += Amount;
+                    break;
+                }
+            case StatsType.Magic:
+                {
+                    Magic += Amount;
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+    public int GetStat(StatsType statsType)
+    {
+        switch (statsType)
+        {
+            case StatsType.Hp:
+                {
+                    return Hp; ;
+                }
+            case StatsType.Defense:
+                {
+                    return Defense; ;
+                }
+            case StatsType.Workers:
+                {
+                    return Workers;
+                }
+            case StatsType.Materials:
+                {
+                    return Materials;
+                }
+            case StatsType.Generals:
+                {
+                    return Generals;
+                }
+            case StatsType.Soldiers:
+                {
+                    return Soldiers;
+                }
+            case StatsType.Mages:
+                {
+                    return Mages;
+                }
+            case StatsType.Magic:
+                {
+                    return Magic;
+                }
+            default:
+                return 0;
+        }
+    }
+}
+
+
+public enum StatsType
+{
+    Hp,
+    Defense,
+    Workers,
+    Materials,
+    Generals,
+    Soldiers,
+    Mages,
+    Magic
 }
