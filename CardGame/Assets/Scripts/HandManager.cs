de4d2cf0f7f7a505d762d4651b818b102cf891b4
davidfrk <davida1d2@gmail.com>
2017-12-16 11:37:25 -0200

    Alterando UI e posição das cartas de acordo com resolução

--- CardGame/Assets/Scripts/HandManager.cs
+++ CardGame/Assets/Scripts/HandManager.cs
@@ -14,13 +14,30 @@ public class HandManager : MonoBehaviour {
     public GameObject CardSlot;
     public List<Transform> CardsTransforms;
 
+    public Vector3 CardsPositionsOnViewport;
+
     private void Awake()
     {
         instance = this;
     }
 
-    public void UpdateCardsPositions()
+    private void Start()
     {
+        UpdateCardsPositions();
+    }
+
+    public void UpdateCardsPositions()
+    {   
+        //Posicionando a mão de acordo com a resolução,  consiste em montar um triÂngulo de acordo com a câmera e escalar ele de modo que um dos lados seja tamanhoDaMão/2
+        Vector3 centerOfCViewport = Camera.main.ViewportPointToRay(0.5f * Vector3.right).direction;
+        Vector3 rightOfViewport = Camera.main.ViewportPointToRay(Vector3.right).direction;
+        float norm = 1/Vector3.Dot(centerOfCViewport, rightOfViewport);
+        rightOfViewport = rightOfViewport * norm;
+        Vector3 centerToRight = rightOfViewport - centerOfCViewport;
+        float handSize = numberOfCards * intervalBetweenCards / 2f;
+        float scale = handSize / centerToRight.magnitude;
+        transform.position = Camera.main.transform.position + Camera.main.ViewportPointToRay(CardsPositionsOnViewport).direction * scale;
+
         float offset = (numberOfCards - 1) * intervalBetweenCards / 2f;
         foreach (Transform transform in CardsTransforms)
         {
