64b89d5b7b3f31c8c2f4b063a47b20088bd6573a
davidfrk <davida1d2@gmail.com>
2018-01-01 11:45:27 -0200

    AdManager

---
+++ CardGame/Assets/Scripts/AdManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour {
+
+	public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            ShowOptions options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                {
+                    Debug.Log("Ad Finished");
+                    break;
+                }
+            case ShowResult.Skipped:
+                {
+                    Debug.Log("Ad Skipped");
+                    break;
+                }
+            case ShowResult.Failed:
+                {
+                    Debug.Log("Ad Failed");
+                    break;
+                }
+        }
+    }
+}
